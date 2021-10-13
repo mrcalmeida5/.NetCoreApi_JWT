8aeaefc355bb1fc59b4437d71600003ab08cf13b
Marco <mrcalmeida5pub@gmail.com>
2021-10-13 10:26:35 +0100

    Add a constructor for Startup class

--- Startup.cs
+++ Startup.cs
@@ -15,16 +15,23 @@ namespace CoreApi_JWT
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.AddJsonFile("AppSettings.json");
-            IConfiguration configuration = configurationBuilder.Build();
+            //IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            //configurationBuilder.AddJsonFile("AppSettings.json");
+            //IConfiguration configuration = configurationBuilder.Build();
 
-            var jwtSettings = new JwtTokenSettings(configuration);
+            var jwtSettings = new JwtTokenSettings(Configuration);
 
             services.AddSingleton<JwtTokenSettings>();
 
